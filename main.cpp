aedf6d391bc2d1ef0abcd0a42b6e0e7f2ec981af
neduosiuemailo <neduosiuemailo@gmail.com>
2012-10-09 13:32:35 +0200

    Patobulinau programa iki tobulumo!

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     cout << "Blablabla" << endl;
-    cout << "Vaje vaje kokia programa" << endl;
+    cout << "Vaje vaje kokia programa!" << endl;
+    cout << "Nu tikrai, be jos gyventi negalima." << endl;
     return 0;
 }
